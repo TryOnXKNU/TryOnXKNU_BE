b241c228f58a3f05d5ac2a9eaf9bcc6df9807fb0
swKim <tjsdnrla1234@gmail.com>
2025-07-01 01:19:20 +0900

    first commit

---
+++ src/main/java/org/example/tryonx/TryOnXApplication.java
@@ -0,0 +1,13 @@
+package org.example.tryonx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryOnXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryOnXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tryonx/TryOnXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tryonx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryOnXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
