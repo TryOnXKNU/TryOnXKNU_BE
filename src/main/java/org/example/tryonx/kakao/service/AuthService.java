98ce18490aeb978921460c756e2219f8a3b73389
chajihyun <gh8526@hanmail.net>
2025-07-06 01:40:27 +0900

    카카오 sdk 번호 오류 수정
    카카오 sdk 번호 오류 수정

--- src/main/java/org/example/tryonx/kakao/service/AuthService.java
+++ src/main/java/org/example/tryonx/kakao/service/AuthService.java
@@ -1,10 +1,9 @@
 package org.example.tryonx.kakao.service;
 
-import org.example.tryonx.kakao.dto.KakaoProfile;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
     ResponseEntity<?> getKakaoUserInfo(String authorizeCode);
-    ResponseEntity<?> kakaoLoginWithSDK(String accessToken, KakaoProfile profile);
+    ResponseEntity<?> kakaoLoginWithSDK(String accessToken);
 
 }
