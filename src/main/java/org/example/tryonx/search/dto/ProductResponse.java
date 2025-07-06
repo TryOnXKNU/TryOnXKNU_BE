c32d858236a66c24c6c60a0804395360b4dbfb04
chajihyun <gh8526@hanmail.net>
2025-07-06 20:45:27 +0900

    검색 초안
    검색 초안

---
+++ src/main/java/org/example/tryonx/search/dto/ProductResponse.java
@@ -0,0 +1,13 @@
+package org.example.tryonx.search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+//@RequiredArgsConstructor
+//@AllArgsConstructor
+//public class ProductResponse {
+//}
---
+++ src/main/java/org/example/tryonx/search/dto/SearchDto.java
@@ -0,0 +1,10 @@
+package org.example.tryonx.search.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchDto {
+    private String keyword;
+}
