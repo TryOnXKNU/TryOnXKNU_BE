0a334125c69a90df518c2ba3482f0b6a6c1a515e
chajihyun <gh8526@hanmail.net>
2025-07-15 21:29:24 +0900

    관리자-회원관리-주문내역
    관리자-회원관리-주문내역

---
+++ src/main/java/org/example/tryonx/admin/dto/MemberOrderHistory.java
@@ -0,0 +1,24 @@
+package org.example.tryonx.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.tryonx.orders.order.domain.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberOrderHistory {
+    private String profileUrl;
+    private String name;
+    private Long memberId;
+    private Integer orderId;
+    private LocalDateTime orderedAt;
+    private Integer productId;
+    private String productName;
+    private BigDecimal price;
+    private OrderStatus orderStatus;
+}
--- src/main/java/org/example/tryonx/admin/service/MemberListService.java
+++ src/main/java/org/example/tryonx/admin/service/MemberListService.java
@@ -4,14 +4,21 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.example.tryonx.admin.dto.MemberInfoDto;
 import org.example.tryonx.admin.dto.MemberListDto;
+import org.example.tryonx.admin.dto.MemberOrderHistory;
 import org.example.tryonx.admin.dto.MemberSearchRequest;
 import org.example.tryonx.admin.specification.MemberSpecification;
 import org.example.tryonx.member.domain.Member;
 import org.example.tryonx.member.repository.MemberRepository;
+import org.example.tryonx.orders.order.domain.Order;
+import org.example.tryonx.orders.order.domain.OrderItem;
+import org.example.tryonx.orders.order.repository.OrderRepository;
+import org.example.tryonx.product.domain.Product;
+import org.example.tryonx.product.domain.ProductItem;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +26,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MemberListService {
     private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
 
     /* 멤버 전체 */
     public List<MemberListDto> getUserList(){
@@ -73,6 +81,35 @@ public class MemberListService {
     public List<Member> searchMembers(MemberSearchRequest request) {
         Specification<Member> spec = MemberSpecification.search(request.getSearchKey(), request.getSearchValue());
         return memberRepository.findAll(spec);
+    }
+
+    /* 멤버별 문의내역 조회 */
+    public List<MemberOrderHistory> getOrderHistoryByMember(Long memberId) {
+        Member member = memberRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("멤버 없음"));
+
+        List<Order> orders = orderRepository.findByMember(member);
+        List<MemberOrderHistory> result = new ArrayList<>();
+
+        for (Order order : orders) {
+            for (OrderItem item : order.getOrderItems()) {
+                ProductItem productItem = item.getProductItem();
+                Product product = productItem.getProduct(); // 여기 주의
+                result.add(new MemberOrderHistory(
+                        member.getProfileUrl(),
+                        member.getName(),
+                        member.getMemberId(),
+                        order.getOrderId(),
+                        order.getOrderedAt(),
+                        product.getProductId(),
+                        product.getProductName(),
+                        product.getPrice(),
+                        order.getStatus()
+                ));
+            }
+        }
+
+        return result;
     }
 
 }
