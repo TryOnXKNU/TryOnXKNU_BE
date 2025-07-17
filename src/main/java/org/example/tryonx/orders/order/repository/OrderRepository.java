8066f987b9dfd14723ec246509e419ae0f339ede
swKim <tjsdnrla1234@gmail.com>
2025-07-18 04:04:33 +0900

    상품 상세 리뷰리스트, 상품 리뷰 전체 조회, 상품 리뷰 필터링

--- src/main/java/org/example/tryonx/orders/order/repository/OrderRepository.java
+++ src/main/java/org/example/tryonx/orders/order/repository/OrderRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByMember(Member member);
     List<Order> findByMemberEmail(String email);
+    Integer countByMember(Member member);
 }
