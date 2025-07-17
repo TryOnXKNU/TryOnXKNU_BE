db2190564041cffb308ea552f9c391a94788092c
swKim <tjsdnrla1234@gmail.com>
2025-07-17 19:23:04 +0900

    profile image

--- src/main/java/org/example/tryonx/member/controller/MemberController.java
+++ src/main/java/org/example/tryonx/member/controller/MemberController.java
@@ -6,10 +6,12 @@ import org.example.tryonx.member.dto.MyInfoResponseDto;
 import org.example.tryonx.member.dto.UpdateMemberRequestDto;
 import org.example.tryonx.member.service.MemberService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -46,5 +48,18 @@ public class MemberController {
         String email = userDetails.getUsername();
         memberService.updateMember(email, dto);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping(value = "/profile-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> updateProfileImage(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestParam("profileImage") MultipartFile profileImage) {
+
+        String email = userDetails.getUsername();
+        try {
+            memberService.updateProfileImage(email, profileImage);
+            return ResponseEntity.ok("프로필 이미지가 성공적으로 업데이트되었습니다.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("프로필 이미지 업데이트 실패: " + e.getMessage());
+        }
     }
 }
