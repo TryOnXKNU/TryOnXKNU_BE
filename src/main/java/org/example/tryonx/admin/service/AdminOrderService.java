e420ef9cc51ff002f1436bcbb3cd72830d8d6e57
chajihyun <gh8526@hanmail.net>
2025-07-30 01:52:55 +0900

    문의 상세 초안만
    문의 상세 초안만

--- src/main/java/org/example/tryonx/admin/service/AdminOrderService.java
+++ src/main/java/org/example/tryonx/admin/service/AdminOrderService.java
@@ -3,12 +3,7 @@ package org.example.tryonx.admin.service;
 import lombok.RequiredArgsConstructor;
 import org.example.tryonx.admin.dto.OrderItemsDto;
 import org.example.tryonx.admin.dto.OrderListDto;
-import org.example.tryonx.exchange.domain.Exchange;
-import org.example.tryonx.exchange.repository.ExchangeRepository;
-import org.example.tryonx.orders.order.domain.Order;
 import org.example.tryonx.orders.order.repository.OrderRepository;
-import org.example.tryonx.returns.domain.Returns;
-import org.example.tryonx.returns.repository.ReturnRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
