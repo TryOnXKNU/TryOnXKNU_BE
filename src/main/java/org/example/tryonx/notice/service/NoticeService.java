9dc78d157e76e207ed9f399462856e5d9d8b0af5
swKim <tjsdnrla1234@gmail.com>
2025-07-30 03:40:34 +0900

    알림 응답 수정

--- src/main/java/org/example/tryonx/notice/service/NoticeService.java
+++ src/main/java/org/example/tryonx/notice/service/NoticeService.java
@@ -3,10 +3,12 @@ package org.example.tryonx.notice.service;
 import org.example.tryonx.member.domain.Member;
 import org.example.tryonx.member.repository.MemberRepository;
 import org.example.tryonx.notice.domain.Notification;
+import org.example.tryonx.notice.dto.NoticeResponseDto;
 import org.example.tryonx.notice.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NoticeService {
@@ -17,10 +19,19 @@ public class NoticeService {
         this.memberRepository = memberRepository;
     }
 
-    public List<Notification> findByEmail(String email) {
+    public List<NoticeResponseDto> findByEmail(String email) {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("해당 이메일의 회원이 없습니다."));
-        return notificationRepository.findByMember(member);
+
+        List<Notification> notifications = notificationRepository.findByMember(member);
+
+        return notifications.stream()
+                .map(n -> new NoticeResponseDto(
+                        n.getNotificationId(),
+                        n.getTitle(),
+                        n.getContent(),
+                        n.getCreatedAt()
+                )).toList();
     }
 
 }
