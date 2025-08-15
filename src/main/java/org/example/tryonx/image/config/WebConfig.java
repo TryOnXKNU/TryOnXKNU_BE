31ce37c341e064d01141d05f97eae33787529a11
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2025-08-16 05:23:05 +0900

    모든 경로 및 메소드 허용
    모든 경로 및 메소드 허용

--- src/main/java/org/example/tryonx/image/config/WebConfig.java
+++ src/main/java/org/example/tryonx/image/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.example.tryonx.image.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations("file:upload/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("*"); // 모든 HTTP 메소드 허용
     }
 }
