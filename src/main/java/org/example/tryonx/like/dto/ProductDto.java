7b63e18ce22c8d3e61e6078bfc4c3532763cfd6f
chajihyun <gh8526@hanmail.net>
2025-08-20 16:08:56 +0900

    좋아요에 할인율, 할인된가격 추가
    좋아요에 할인율, 할인된가격 추가

--- src/main/java/org/example/tryonx/like/dto/ProductDto.java
+++ src/main/java/org/example/tryonx/like/dto/ProductDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.example.tryonx.product.domain.Product;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Getter
 @AllArgsConstructor
@@ -14,15 +15,31 @@ public class ProductDto {
     private String imageUrl;
     private BigDecimal price;
     private Long likeCount;
+    private BigDecimal discountRate;
+    private BigDecimal discountPrice;
 
     public static ProductDto of(Product p, long likeCount) {
         String imageUrl = p.getImages().isEmpty() ? null : p.getImages().get(0).getImageUrl();
+
+        BigDecimal price = p.getPrice();
+        BigDecimal discountRate = p.getDiscountRate();
+        BigDecimal discountPrice = price;
+
+        if (discountRate != null && discountRate.compareTo(BigDecimal.ZERO) > 0) {
+            BigDecimal rate = discountRate.divide(BigDecimal.valueOf(100)); // %를 소수로 변환
+            discountPrice = price
+                    .multiply(BigDecimal.ONE.subtract(rate))
+                    .setScale(0, RoundingMode.HALF_UP);
+        }
+
         return new ProductDto(
                 p.getProductId(),
                 p.getProductName(),
                 imageUrl,
                 p.getPrice(),
-                likeCount
+                likeCount,
+                discountRate,
+                discountPrice
         );
     }
 
