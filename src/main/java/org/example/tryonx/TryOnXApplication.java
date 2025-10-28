c6b22281dc25938e0ec5960c0f79395f597d337e
chajihyun <gh8526@hanmail.net>
2025-10-28 21:47:35 +0900

    한국 시간으로 설정
    한국 시간으로 설정

--- src/main/java/org/example/tryonx/TryOnXApplication.java
+++ src/main/java/org/example/tryonx/TryOnXApplication.java
@@ -1,10 +1,19 @@
 package org.example.tryonx;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TryOnXApplication {
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("JVM TimeZone set to: " + TimeZone.getDefault().getID());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TryOnXApplication.class, args);
